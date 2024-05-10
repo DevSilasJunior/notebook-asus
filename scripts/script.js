7a1ccbc776a53bd3a902197ef808be692d7979cc
DevSilasJúnior <silasfigueredodasilvajunior@gmail.com>
2024-05-10 15:18:13 -0300

    versio finaly divulgação asus

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,10 @@
 window.addEventListener("scroll", function(){
+    const width = document.body.clientWidth
     var opcao1 = document.querySelector(".op1")
     var opcao2 = document.querySelector(".op2")
     var opcao3 = document.querySelector(".op3")
     var menu = document.getElementsByName("nav#menu")
-    if (this.window.scrollY > 50){
-        opcao3.classList.add("movit-menu1")
-    } else{
-        opcao3.classList.remove("movit-menu1")
-    }
-    if (this.window.scrollY > 150){
-        opcao3.classList.add("movit-menu2")
-        opcao2.classList.add("movit-menu1")
-    } else{
-        opcao3.classList.remove("movit-menu2")
-        opcao2.classList.remove("movit-menu1")
-    }
-    if (this.window.scrollY > 250){
-        opcao1.classList.add("movit-menu1")
-        opcao2.classList.add("movit-menu2")
-        opcao3.classList.add("movit-menu3")
-        
-    } else{
-        opcao1.classList.remove("movit-menu1")
-        opcao2.classList.remove("movit-menu2")
-        opcao3.classList.remove("movit-menu3")
-    }
     //Media max-width: 877
-    const width = document.body.style.width
-    width = Number(width.value)
     if (width < 878){
         if (this.window.scrollY > 20){
             opcao3.classList.add("movit-menu1")
@@ -42,6 +19,29 @@ window.addEventListener("scroll", function(){
             opcao2.classList.remove("movit-menu1")
         }
         if (this.window.scrollY > 60){
+            opcao1.classList.add("movit-menu1")
+            opcao2.classList.add("movit-menu2")
+            opcao3.classList.add("movit-menu3")
+            
+        } else{
+            opcao1.classList.remove("movit-menu1")
+            opcao2.classList.remove("movit-menu2")
+            opcao3.classList.remove("movit-menu3")
+        }
+    }else{
+        if (this.window.scrollY > 50){
+            opcao3.classList.add("movit-menu1")
+        } else{
+            opcao3.classList.remove("movit-menu1")
+        }
+        if (this.window.scrollY > 150){
+            opcao3.classList.add("movit-menu2")
+            opcao2.classList.add("movit-menu1")
+        } else{
+            opcao3.classList.remove("movit-menu2")
+            opcao2.classList.remove("movit-menu1")
+        }
+        if (this.window.scrollY > 250){
             opcao1.classList.add("movit-menu1")
             opcao2.classList.add("movit-menu2")
             opcao3.classList.add("movit-menu3")
